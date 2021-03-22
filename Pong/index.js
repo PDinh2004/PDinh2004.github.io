306b4746b45db7cb7d339ba13596d5ac7d607c4e
PDinh2004 <phillipdinh04@gmail.com>
2021-03-22 23:38:30 +0000

    adding Pong

--- Pong/index.js
+++ Pong/index.js
@@ -27,20 +27,14 @@ function runProgram(){
       "Down": 83
   };
 
-  var doubleMaxSpeed = 5;
-
   // Game Item Objects
-
-    var paddle1PositionX = 30; // the x-coordinate location for the paddle1
-    var paddle1PositionY = 180; // the y-coordinate location for the paddle1
-    var paddle2PositionX = 390; // the x-coordinate location for the paddle2
-    var paddle2PositionY = 180; // the y-coordinate location for the paddle2
-    var paddle1SpeedY = 0; // the speed for the paddle1 along the y-axis
-    var paddle2SpeedY = 0; // the speed for the paddle2 along the y-axis
-    var pongPositionX = 210;
-    var pongPositionY = 200;
-    var pongSpeedX = 0;
-    var pongSpeedY = 0;
+  var pad1 = factoryMachine("#paddle1");
+  var pad2 = factoryMachine("#paddle2");
+  var pong = factoryMachine("#pong");
+  var points1 = 0;
+  var points2 = 0;
+  $("#scoreOne").text(points1);
+  $("#scoreTwo").text(points2);
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -61,7 +55,26 @@ function runProgram(){
     repositionGameItem();
     redrawGameItem();
     borderBoard();
-  }
+    increaseScore();
+
+    // Determines whether or not the pong reflects
+    if (doCollide(pad1, pong)) {
+        pong.speedX = -pong.speedX;
+    }
+    if (doCollide(pad2, pong)) {
+        pong.speedX = -pong.speedX;
+    }
+
+    if (points1 >= 3) {
+        $("h2").text("GAME OVER, PLAYER 1 WINS!");
+        pong.speedX = 0;
+        pong.speedY = 0;
+    } else if (points2 >= 3) {
+        $("h2").text("GAME OVER, PLAYER 2 WINS!");
+        pong.speedX = 0;
+        pong.speedY = 0;
+    }
+}
   
   /* 
   Called in response to events.
@@ -71,10 +84,10 @@ function runProgram(){
   function handleKeyDown1(event) {
         if (event.which === KEY1.Up){
             console.log("Pressed Up");
-            paddle1SpeedY = -5;
+            pad1.speedY = -5;
         } else if (event.which === KEY1.Down){
             console.log("Pressed Down");
-            paddle1SpeedY = 5;
+            pad1.speedY = 5;
         }
   }
 
@@ -82,10 +95,10 @@ function runProgram(){
   function handleKeyUp1(event) {
         if (event.which === KEY1.Up){
           console.log("Lifted Up");
-          paddle1SpeedY = 0;
+          pad1.speedY = 0;
       } else if (event.which === KEY1.Down){
           console.log("Lifted Down");
-          paddle1SpeedY = 0;
+          pad1.speedY = 0;
       }
   }
 
@@ -93,10 +106,10 @@ function runProgram(){
   function handleKeyDown2(event){
         if (event.which === KEY2.Up){
           console.log("Pressed Up1");
-          paddle2SpeedY = -5;
+          pad2.speedY = -5;
       } else if (event.which === KEY2.Down){
           console.log("Pressed Down1");
-          paddle2SpeedY = 5;
+          pad2.speedY = 5;
       }
   }
 
@@ -104,49 +117,123 @@ function runProgram(){
   function handleKeyUp2(event){
         if (event.which === KEY2.Up){
           console.log("Lifted Up");
-          paddle2SpeedY = 0;
+          pad2.speedY = 0;
       } else if (event.which === KEY2.Down){
           console.log("Lifted Down");
-          paddle2SpeedY = 0;
+          pad2.speedY = 0;
       }
   }
 
-function decideSpeed(){
-    return Math.random() * doubleMaxSpeed/2 - doubleMaxSpeed;
+// Randomizes the ball's direction at the start
+function decideSpeed() {
+  var randomNum = Math.floor(Math.random() * Math.floor(2));
+  if (randomNum === 0) {
+      pong.speedX = 4;
+      pong.speedY = 4;
+      pong.x = 210;
+      pong.y = 200;
+  } else {
+      pong.speedX = -4;
+      pong.speedY = 4;
+      pong.x = 210;
+      pong.y = 200;
+  }
 }
 
-pongSpeedX = decideSpeed();
-pongSpeedY = decideSpeed();
+decideSpeed();
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+// Create Objects
+function factoryMachine($id){
+  var gameObject = {};
+  gameObject.id = $id;
+  gameObject.x = Number($($id).css('left').replace(/[^-\d\.]/g, ''));
+  gameObject.y = Number($($id).css('top').replace(/[^-\d\.]/g, ''));
+  gameObject.width = $($id).width();
+  gameObject.height = $($id).height();
+  gameObject.speedX = 0;
+  gameObject.speedY = 0;
+  return gameObject;
+}
+
+console.log(pad1);
+console.log(pad2);
+
 function repositionGameItem(){
-    paddle1PositionY += paddle1SpeedY; // Repositions paddle1 in the y-direction
-    paddle2PositionY += paddle2SpeedY; // Repositions paddle2 in the y-direction
-    pongPositionX += pongSpeedX;
-    pongPositionY += pongSpeedY;
+    pad1.y += pad1.speedY; // Repositions paddle1 in the y-direction
+    pad2.y += pad2.speedY; // Repositions paddle2 in the y-direction
+    pong.x += pong.speedX; // Repositions pong in the x-direction
+    pong.y += pong.speedY; // Repositions pong in the y-direction
   }
 
 function redrawGameItem(){
-    $("#paddle1").css("top", paddle1PositionY); // Redraws box up or down
-    $("#paddle2").css("top", paddle2PositionY); // Redraws box2 up or down
+    $("#paddle1").css("top", pad1.y); // Redraws box up or down
+    $("#paddle2").css("top", pad2.y); // Redraws box2 up or down
+    $("#pong").css("top", pong.y);
+    $("#pong").css("left", pong.x);
   }
   
+// Keeps all the moving elements within the board
 function borderBoard(){
-    if (paddle1PositionY > 355) {
-        paddle1PositionY = 355;
-    } else if (paddle1PositionY < 5) {
-        paddle1PositionY = 5;
+    if (pad1.y > 355) {
+        pad1.y = 355;
+    } else if (pad1.y < 5) {
+        pad1.y = 5;
     }
-    if (paddle2PositionY > 355) {
-        paddle2PositionY = 355;
-    } else if (paddle2PositionY < 5) {
-        paddle2PositionY = 5;
+    if (pad2.y > 355) {
+        pad2.y = 355;
+    } else if (pad2.y < 5) {
+        pad2.y = 5;
+    }
+    if (pong.y > 420) {
+        pong.speedY = -pong.speedY;
+    } else if (pong.y < 0) {
+        pong.speedY = -pong.speedY;
     }
   }
+
+// Increases the score of the game
+function increaseScore(){
+    if (pong.x > 420) {
+        var pointsIncrease = ++points1;
+        $("#scoreOne").text(pointsIncrease);
+        pong.x = 210;
+        pong.speedX = -pong.speedX;
+    } 
+    if (pong.x < 0) {
+        var pointsIncrease = ++points2;
+        $("#scoreTwo").text(pointsIncrease);
+        pong.x = 210;
+        pong.speedX = -pong.speedX;
+    }
+}
+
+// Determines if the pong collides w/ either paddle
+function doCollide(pad, pong) {
+
+    pad.left = pad.x;
+    pad.top = pad.y;
+    pad.right = pad.x + pad.width;
+    pad.bottom = pad.y + pad.height;
   
+    pong.left = pong.x;
+    pong.top = pong.y;
+    pong.right = pong.x + pong.width;
+    pong.bottom = pong.y + pong.height;
+  
+    // TODO: Return true if they are overlapping, false otherwise
+	
+    var result = (pad.left < pong.right && pad.right > pong.left && pad.top < pong.bottom && pad.bottom > pong.top) ? true : false;
+    if (result) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
