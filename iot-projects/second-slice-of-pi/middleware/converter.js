c0cd2a84a092d572ed38bc464117b19c5e39fc3c
PDinh2004 <phillipdinh04@gmail.com>
2021-11-18 17:39:56 -0600

    secondpi

--- iot-projects/second-slice-of-pi/middleware/converter.js
+++ iot-projects/second-slice-of-pi/middleware/converter.js
@@ -3,6 +3,29 @@ const json2html = require('node-json2html');
 module.exports = function() {
 	return function (req, res, next) {
 		// TODO 2: Create the converter function
+		if (req.result) { 
+		        res.send(req.result); 
+		};
 		
+		next();
+		
+		if (req.accepts('html')){
+		        let transform = {'<>': 'div', 'html': [
+                            {'<>': 'p', 'html': [
+                                {'<>': 'b', 'html': 'Name: '},
+                                {'<>': 'p', 'html': '${name}'}
+                            ]},
+                            {'<>': 'p', 'html': [
+                                {'<>': 'b', 'html': 'Description: '},
+                                {'<>': 'p', 'html': '${description}'}
+                            ]},
+                            {'<>': 'p', 'html': [
+                                {'<>': 'b', 'html': 'Value: '},
+                                {'<>': 'p', 'html': '${value}'}
+                            ]}
+                            
+                            res.send(json2html.transform(req.result, transform));
+                        ]};
+		}
 	};
 };
