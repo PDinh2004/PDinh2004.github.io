ebe95bfa1d00cb6d7e007c26720ed0f463ede498
PDinh2004 <phillipdinh04@gmail.com>
2021-12-16 17:57:39 -0600

    auto v.1

--- iot-projects/automatic-routing/routes/actuators.js
+++ iot-projects/automatic-routing/routes/actuators.js
@@ -4,20 +4,7 @@ const express = require('express'),
 	router = express.Router(),
 	resources = require('./../resources/model');
 
-router.route('/').get(function (req, res, next) {
-	req.result = resources.pi.actuators;
-	next();
-});
-
-router.route('/leds').get(function (req, res, next) {
-	req.result = resources.pi.actuators.leds;
-	next();
-});
-
-router.route('/leds/:id').get(function (req, res, next) {
-	req.result = resources.pi.actuators.leds[req.params.id];
-	next();
-}).put(function(req, res, next){
+router.route('/leds/:id').put(function(req, res, next){
         var led = resources.pi.actuators.leds[req.params.id];
 	led.value = req.body.value;
 	req.result = led;
--- iot-projects/automatic-routing/servers/http.js
+++ iot-projects/automatic-routing/servers/http.js
@@ -1,6 +1,7 @@
+const createRouter = require('./../routes/automate');
+const resources = require('./../resources/model');
 var converter = require('./../middleware/converter');
 var bodyParser = require('body-parser');
-var sensorRoutes = require('./../routes/sensors');
 var actuatorRoutes = require('./../routes/actuators');
 
 const express = require('express'),
@@ -9,17 +10,9 @@ const express = require('express'),
 var app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/pi/sensors', sensorRoutes);
 app.use('/pi/actuators', actuatorRoutes);
 
-app.get("/", function(req, res){
-        res.send("Someone accessed the homepage!");
-})
-
-app.get("/pi", function(req, res){
-        res.send("Someone accessed the Pi page!");
-})
-
+app.use('/', createRouter(resources));
 app.use(converter());
 module.exports = app;
 // I have looked through all the files
